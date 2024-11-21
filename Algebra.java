aa9f4cb29e80b75d52d5e7b524fabcebb120f573
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-11-21 20:45:29 +0200

    2 try algebra

--- Algebra.java
+++ Algebra.java
@@ -3,6 +3,8 @@
 // Math.sqrt. All the functions in this class operate on int values and
 // return int values.
 
+import javax.naming.NameNotFoundException;
+
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
@@ -26,35 +28,107 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// loop too add
-		int add = x2;
-		for(int i = 0; i < x1; i++)
-		{add ++;}
-		return add;
-	}
+		// number > 0
+		if ((x1 > 0) & (x2 > 0)) {
+			int add = x2;
+			for(int i = 0; i < x1; i++)
+				{add ++;}
+			return add;}
+		
+		// one > 0 and the other < 0
+		if ((x1 > 0) & (x2 < 0)) {
+			int add = x1;
+			for(int i = 0; i < x2; i++)
+				{add --;}
+			return add;
+		}
+
+		// one > 0 and the other < 0
+		if ((x1 < 0) & (x2 > 0)) {
+			int add = x2;
+			for(int i = 0; i < x1; i++)
+				{add --;}
+			return add;
+		}
+
+		// both < 0
+		if ((x1 < 0) & (x2 < 0)) {
+			int add = x2;
+			for(int i = 0; i < x1; i++)
+				{add --;}
+			return add;}
+		}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		// loop to minus
-		int minus1 = x1; 
-		for(int i = 0; i < x2 ;i++){
-			minus1 = minus1 - 1;}
-		return minus1;
-	}
+		// number > 0
+		if ((x1 > 0) & (x2 > 0)) {
+			int minus1 = x1; 
+			for(int i = 0; i < x2 ;i++){
+				minus1--;}
+			return minus1;
+		}
+
+		// one > 0 and the other < 0
+		if ((x1 > 0) & (x2 < 0)) {
+			int minus1 = x1;
+			for(int i = x2; i < 0 ;i++){
+				minus1 ++;}
+			return minus1;
+		}
+
+		// one > 0 and the other < 0
+		if ((x1 < 0) & (x2 > 0)) {
+			int minus1 = x1;
+			for(int i = 0; i < x2 ;i++){
+				minus1 --;}
+			return minus1;
+		}
+
+		// both < 0
+		if ((x1 < 0) & (x2 < 0)) {
+			int minus1 = x1;
+			for(int i = x2; i < 0 ;i++){
+				minus1 = minus1 ++;}
+			return minus1;}	
+			}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		// loop to times
-		int times1 = x2;
-		for(int i = 0; i < x1 - 1; i++){
-			times1 = plus(x2, times1);
+		if ((x1 == 0 || x2 == 0)) {
+			return 0;
 		}
+
+		if ((x1 > 0 & x2 > 0)) {
+			int times1 = x2;
+			for(int i = 0; i < x1 - 1; i++){
+			times1 = plus(x2, times1);}
 		return times1;
+		}
+
+		else{int times1 = x2;
+			for(int i = 0; i < x1 - 1; i++){
+				times1 = plus(x2, times1);}
+			int minus_time = minus(0, times1);
+			return minus_time;
+		}
+		
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
 		// loop for pow
+		if ((n == 0)) {
+			return 1;
+		}
 		int pow1 = x;
+		// number < 0
+		if (pow1 < 0) {
+			pow1 = times(-1, pow1);
+		}
+		// number > 0
 		for(int i = 0; i < n -1 ;i++){
 			pow1 = times(pow1, x);
 		}
@@ -64,16 +138,42 @@ public class Algebra {
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		// loop for divers
-		if (x2 > x1) {
-			return 0;}
-		int divers1 = x2;
-		int much = 0;
-		for(int i =0; divers1 < x1 + 1; i++){
-			much ++;
-			divers1 = divers1 + x2;
+		// numbers > 0
+		if (x1 > 0 & x2 > 0) {
+			if (x2 > x1){return 0;}
+			int divers1 = x2;
+			int much = 0;
+			for(int i =0; divers1 < x1 + 1; i++){
+				much ++;
+				divers1 = divers1 + x2;}
+			return much;
 		}
-		return much;
-	}
+
+		// numbers < 0
+		if ((x1 < 0 & x2 < 0)) {
+			x1 = times(x1, -1);
+			x2 = times(-1, x2);
+			if (x2 > x1){return 0;}
+			int divers1 = x2;
+			int much = 0;
+			for(int i =0; divers1 < x1 + 1; i++){
+				much ++;
+				divers1 = divers1 + x2;}
+			return much;}
+
+		// if only 1 is > 0
+		if (x1 < 0 || x2 < 0) {
+			x1 = times(x1, -1);
+			x2 = times(-1, x2);
+			if (x2 > x1){return 0;}
+			int divers1 = x2;
+			int much = 0;
+			for(int i =0; divers1 < x1 + 1; i++){
+				much ++;
+				divers1 = divers1 + x2;}
+				int minus_times = times(much, -1);
+			return minus_times ;}
+		
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
@@ -85,13 +185,17 @@ public class Algebra {
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		// loop for sqrt
+		if ( x < 0) {
+			System.out.println("minus cant be sqrt");
+		}
+		else{
 		int new_squrt = 1;
 		for(int i = 0; i < x; i++){
 			if (i *i == x) {
-				new_squrt = i;
+				new_squrt = i;}
 			}
+			return new_squrt;
 		}
 		
-		return new_squrt;
 	}	  	  
 }
