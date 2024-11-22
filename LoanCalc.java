738d992a37ebace29be855b63064ce9f8fb007d1
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-11-22 11:41:28 +0200

    first try loancalc

--- LoanCalc.java
+++ LoanCalc.java
@@ -29,7 +29,12 @@ public class LoanCalc {
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
 		// Replace the following statement with your code
-		return 0;
+		double left_to_pay = loan;
+		double interest_rate = 1 + (rate/100);
+		for(int j = 0; j<n; j++){
+			left_to_pay = (left_to_pay - payment) * interest_rate;
+		}
+		return left_to_pay;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -39,7 +44,19 @@ public class LoanCalc {
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
 		// Replace the following statement with your code
-		return 0;
+		double try_payment = 10;
+		epsilon = 10;
+		int increment = 5;
+		int stepcounter =0;
+		double endB = loan;
+		while (endB >= epsilon) {
+			endB = endBalance(loan, rate, increment, try_payment);
+			try_payment = try_payment + increment;
+			stepcounter++;
+		}
+		System.out.println("Periodical payment, using brute force: " + try_payment);
+		System.out.println("number of iterations: " + stepcounter);
+		return try_payment ;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -49,6 +66,24 @@ public class LoanCalc {
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
         // Replace the following statement with your code
-		return 0;
+		double try_payment = (loan + epsilon)/2;
+		double endB = loan;
+		epsilon = 10;
+		double last_try = loan;
+		int stepcounter = 0;
+		while (endB <= epsilon) {
+			stepcounter++;
+			endB = endBalance(loan, rate, n, try_payment);
+			if (endB < 0) {
+				last_try = try_payment;
+				try_payment = (epsilon + try_payment)/2;
+			}
+			else{
+				try_payment = (try_payment + last_try)/2;
+			}
+		}
+		System.out.println("Periodical payment, using brute force: " + try_payment);
+		System.out.println("number of iterations: " + stepcounter);
+		return try_payment;
     }
 }
