d2e2946bd69457f5dea4d65f2d35854e8dc1f4d1
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-11-24 15:19:08 +0200

    again

--- LoanCalc.java
+++ LoanCalc.java
@@ -44,7 +44,7 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
 	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
+		// Replace the following statement with code
 		double try_payment = loan/n;
 		double increment = 0.001;
 		iterationCounter = 0;
